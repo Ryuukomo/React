9320d8f9b6d3c0c70c80b3ec231c8d194df576ea
Ryuukomo <98666355+Ryuukomo@users.noreply.github.com>
2026-09-14 20:18:52 -0300

    pokedex consertada

--- consumoAPI/src/Pokedex.jsx
+++ consumoAPI/src/Pokedex.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 
 function Pokedex() {
 
-    const [pokemon, setPokemon] = useState({})
+    const [pokemon, setPokemon] = useState(null)
     const [pesquisa, alteraPesquisa] = useState("")
 
 
@@ -12,12 +12,7 @@ function Pokedex() {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + nome)
         const data = await response.json()
 
-
-
         setPokemon(data)
-
-
-
 
     }
 
@@ -35,22 +30,19 @@ function Pokedex() {
 
 
 
+            {
+                pokemon == null ?
+                    <p> vazio</p>
+                :
+                    <div>
+                        <h2>Nome: {pokemon.name} </h2>
+                        <p>Tipo: {pokemon.types?.map(i => i.type?.name + " / ")} </p>  {/* "/" serve para separar tipo  */}
+                        <img width="100px" src={pokemon.sprites?.front_shiny} />
+                    </div>
 
-            <h2>Nome: {pokemon.name} </h2>
-            <p>Tipo: {pokemon.types[0]?.type?.name} </p>
-            <img width={"100px"} src={pokemon.sprites?.front_shiny} />
+            }
 
-
-            {/* <h2>Nome: {pokemon.name} </h2>
-             <p>Tipo: {pokemon.types?.map(i=> i.type.name + " )} </p>
-             <img width={"100px"} src={pokemon.sprites?.front_shiny}/> */}
-
-
-
-
-
-
-
+           
         </div>
     );
 }
