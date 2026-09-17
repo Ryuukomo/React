1f2376e9a687fda0bbcd437e524d41c86ebc127e
Ryuukomo <98666355+Ryuukomo@users.noreply.github.com>
2026-09-17 15:28:07 -0300

    css criado

--- conexaosupabase/src/Empresas.jsx
+++ conexaosupabase/src/Empresas.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
+import "./Empresas.css"
 
 function Empresas() {
 
     const [empresa, alteraEmpresas] = useState([])
     const [funcionarios, alteraFuncionarios] = useState([])
+    const [exibeFuncionarios, alteraExibeFuncionarios] = useState(false)
+    const [exibeEmpresas, alteraExibeEmpresas] = useState(true)
 
     async function captarEmpresas() {
 
@@ -15,12 +18,34 @@ function Empresas() {
     async function captarFuncionarios() {
 
         const { error, data } = await supabase.from("funcionario").select(
-           `*,
+            `*,
             
             empresas ( nome, endereco )`)
 
         console.log(data)
         alteraFuncionarios(data)
+    }
+    async function buscaUsuariosPorEmpresa(id_empresa) {
+        const { error, data } = await supabase.from("funcionario").select("*,empresas(*)").eq("id_empresa", id_empresa)
+
+        console.log(data)
+        alteraFuncionarios(data)
+        alteraVisualizacao()
+
+    }
+
+    async function alteraVisualizacao(id_empresa) {
+
+        if (exibeEmpresas == true) {
+            alteraExibeEmpresas(false)
+            alteraExibeFuncionarios(true)
+        }
+        else {
+            alteraExibeEmpresas(true)
+            alteraExibeFuncionarios(false)
+
+        }
+
     }
 
     useEffect(() => {
@@ -30,61 +55,92 @@ function Empresas() {
     return (
         <div>
 
-            <h1>Empresas</h1>
-            <p>Consulta na tabela empresas e fncionarios </p>
+            {
+                exibeEmpresas == true ?
+                    <div>
+                        <h1>Empresas</h1>
+                        <p>Consulta na tabela empresas e fncionarios </p>
 
-            <table border="true">
+                        <table border="true">
 
-                <tr>
-                    <td>ID</td>
-                    <td>NOME</td>
-                    <td>CNPJ</td>
-                    <td>ENDEREÇO</td>
-                </tr>
-                {
-                    empresa.map(i =>
+                            <tr>
+                                <td>ID</td>
+                                <td>NOME</td>
+                                <td>CNPJ</td>
+                                <td>ENDEREÇO</td>
+                                <td>AÇÕES</td>
+                            </tr>
+                            {
+                                empresa.map(i =>
 
-                        <tr>
-                            <td> {i.id}</td>
-                            <td> {i.nome}</td>
-                            <td> {i.cnpj}</td>
-                            <td> {i.endereco}</td>
-                        </tr>
+                                    <tr>
+                                        <td> {i.id}</td>
+                                        <td> {i.nome}</td>
+                                        <td> {i.cnpj}</td>
+                                        <td> {i.endereco}</td>
+                                        <td><button onClick={() => buscaUsuariosPorEmpresa(i.id)}>Ver funcionários</button></td>
+                                    </tr>
 
-                    )
-                }
+                                )
+                            }
 
-            </table>
+                        </table>
 
-            <br />            <br />            <br />            <br />            <br />            <br />
+                        <br />            <br />            <br />            <br />            <br />            <br />
 
-            <table border="true">
 
-                <tr>
-                    <td>ID</td>
-                    <td>NOME DA EMPRESA </td>
-                    <td>NOME </td>
-                    <td>ENDERECO </td>
-                    <td>CARGO</td>
-                    <td>CONTATO</td>
+                    </div>
 
-                </tr>
-                {
-                    funcionarios.map(i =>
+                    :
+                    <></>
+            }
 
-                        <tr>
-                            <td> {i.id}</td>
-                            <td> {i.empresas.nome}</td>
-                            <td> {i.nome}</td>
-                            <td> {i.empresas.endereco}</td>
-                            <td> {i.cargo == 0 ? "Admin" : "funcionario"}</td>
-                            <td> {i.contato}</td>
-                        </tr>
+            {
+                exibeFuncionarios == true ?
 
-                    )
-                }
+                    <div>
 
-            </table>
+                        <h2>Funcionários</h2>
+
+                        <button onClick={alteraVisualizacao}>Voltar</button>
+                        <button>Adicionar Novo</button>
+
+                        <table border="true">
+
+                            <tr>
+                                <td>ID</td>
+                                <td>NOME DA EMPRESA </td>
+                                <td>NOME </td>
+                                <td>ENDERECO </td>
+                                <td>CARGO</td>
+                                <td>CONTATO</td>
+
+                            </tr>
+
+
+                            {
+
+                                funcionarios.map(i =>
+
+                                    <tr>
+                                        <td> {i.id}</td>
+                                        <td> {i.empresas.nome}</td>
+                                        <td> {i.nome}</td>
+                                        <td> {i.empresas.endereco}</td>
+                                        <td> {i.cargo == 0 ? "Admin" : "funcionario"}</td>
+                                        <td> {i.contato}</td>
+                                    </tr>
+
+                                )
+                            }
+
+                        </table>
+                    </div>
+
+                    :
+                    <></>
+            }
+
         </div>
 
     );
